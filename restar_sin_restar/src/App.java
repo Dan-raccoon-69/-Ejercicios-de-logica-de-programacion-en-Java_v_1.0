b37b74545f80f7f35891371297618678f5302d23
Daniel De La Cruz <gustavodelacruz2010@gmail.com>
2023-01-06 14:17:26 -0600

    Cambios

--- restar_sin_restar/src/App.java
+++ restar_sin_restar/src/App.java
@@ -1,11 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println(restarSinRestar(5, 1));
-        
+
+        System.out.println(restarSinRestar(5, 1)); // 4
+        System.out.println(restarSinRestar(15, 10)); // 5
+        System.out.println(restarSinRestar(10, 0)); // 10
+        System.out.println(restarSinRestar(10, 20)); // -10
+        System.out.println(restarSinRestar(-10, -13)); // 3
+        System.out.println(restarSinRestar(-15, -20)); // 5
+        System.out.println(restarSinRestar(-4, -3)); // -1
+        System.out.println(restarSinRestar(-20, -15)); // -5
+        System.out.println(restarSinRestar(-4, 2)); // -6
+        System.out.println(restarSinRestar(-4, 3)); // -7
+        System.out.println(restarSinRestar(10, -20)); // 30
+        System.out.println(restarSinRestar(5, -30)); // 35
+
     }
 
-    public static int restarSinRestar(int valor1, int valor2){
-        return valor1 + ((-1)*valor2);
+    public static int restarSinRestar(int valor1, int valor2) {
+        System.out.println("Resta de: " + valor1 + " (-) " + valor2);
+        return valor1 + ((-1) * valor2);
     }
 }
